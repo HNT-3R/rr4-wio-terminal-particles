cd7e8445c14f5e7ef9854ba0b9b90bbc0206e57e
Remilia <remfem@proton.me>
2026-04-21 14:27:10 +0200

    Screen refresh

--- wio-terminal-particle-proj/wio-terminal-particle-proj.ino
+++ wio-terminal-particle-proj/wio-terminal-particle-proj.ino
@@ -1,53 +1,17 @@
-#include <Seeed_HM330X.h>
-
-/*
-    basic_demo.ino
-    Example for Seeed PM2.5 Sensor(HM300)
-
-    Copyright (c) 2018 Seeed Technology Co., Ltd.
-    Website    : www.seeed.cc
-    Author     : downey
-    Create Time: August 2018
-    Change Log :
-
-    The MIT License (MIT)
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    THE SOFTWARE.
-*/
 
 #include <Seeed_HM330X.h>
-#include"TFT_eSPI.h"
+#include "TFT_eSPI.h"
+#include <SparkFunBQ27441.h>
 
-
-
-#ifdef  ARDUINO_SAMD_VARIANT_COMPLIANCE
-    #define SERIAL_OUTPUT tft
-#else
-    #define SERIAL_OUTPUT tft
-#endif
+// Serial-Monitor für Debug
+#define DEBUG_OUTPUT Serial
 
 HM330X sensor;
 TFT_eSPI tft;
 uint8_t buf[30];
 
 //PMXXX mit XXX = Größe in Mikrometer der Partikel
-const char* str[] = {"sensor num: ", "PM1.0 conc(CF=1,SPM): ",
+const char* str[] = {"PM1.0 conc(CF=1,SPM): ",
                      "PM2.5 conc(CF=1,SPM): ",
                      "PM10 conc(CF=1,SPM): ",
                      "PM1.0 conc(AE): ",
@@ -55,83 +19,85 @@ const char* str[] = {"sensor num: ", "PM1.0 conc(CF=1,SPM): ",
                      "PM10 conc(AE): ",
                     };
 
-
-HM330XErrorCode print_result(const char* str, uint16_t value, int line) {
+void print_result(const char* str, uint16_t value, int line) {
     if (NULL == str) {
-        return ERROR_PARAM;
+        return;
     }
-    //Aufloesung: 320 x 240
     char result[100];
     sprintf(result, "%s %d", str, value);
-    SERIAL_OUTPUT.drawString("Units: ug/m^3", 10, 5);
-    SERIAL_OUTPUT.drawString("AE = Atmospheric environment", 10, 20);
-    SERIAL_OUTPUT.drawString("SPM = Standard particulate matter", 10, 35);
-    SERIAL_OUTPUT.drawString(result, 10, 40 + line*20);
-    return NO_ERROR;
+    tft.drawString(result, 10, 60 + line * 25);
 }
 
 /*parse buf with 29 uint8_t-data*/
-HM330XErrorCode parse_result(uint8_t* data) {
+void parse_result(uint8_t* data) {
     uint16_t value = 0;
     if (NULL == data) {
-        return ERROR_PARAM;
+        return;
     }
-    for (int i = 1; i < 8 + 4; i++) {
-        value = (uint16_t) data[i * 2] << 8 | data[i * 2 + 1];
-        print_result(str[i - 1], value, i);
-
+    
+    tft.fillRect(10, 60, 310, 180, TFT_BLACK);
+    
+    for (int i = 1; i <= 6; i++) { 
+        value = (uint16_t)data[i * 2] << 8 | data[i * 2 + 1];
+        print_result(str[i - 1], value, i - 1);
     }
-
-    return NO_ERROR;
 }
 
-HM330XErrorCode parse_result_value(uint8_t* data) {
-    if (NULL == data) {
-        return ERROR_PARAM;
-    }
-    for (int i = 0; i < 28; i++) {
-        //SERIAL_OUTPUT.print(data[i], HEX);
-        //SERIAL_OUTPUT.print("  ");
-        if ((0 == (i) % 5) || (0 == i)) {
-            //SERIAL_OUTPUT.println("");
-        }
-    }
-    uint8_t sum = 0;
-    for (int i = 0; i < 28; i++) {
-        sum += data[i];
-    }
-    if (sum != data[28]) {
-        //SERIAL_OUTPUT.println("wrong checkSum!!");
-    }
-    //SERIAL_OUTPUT.println("");
-    return NO_ERROR;
-}
-
-
-/*30s*/
 void setup() {
-    SERIAL_OUTPUT.begin(115200);
+    unsigned int soc = lipo.soc();
+
+    DEBUG_OUTPUT.begin(115200);
     delay(100);
-    SERIAL_OUTPUT.println("Serial start");
+    DEBUG_OUTPUT.println("Wio Terminal PM2.5 Sensor Starting...");
+    
+    // Sensorinitialisierung
     if (sensor.init()) {
-        SERIAL_OUTPUT.println("HM330X init failed!!");
+        DEBUG_OUTPUT.println("HM330X init failed!!");
         while (1);
     }
+    DEBUG_OUTPUT.println("HM330X initialized successfully");
 
+    // Displayinitialisierung
     tft.begin();
     tft.setRotation(3);
-    digitalWrite(LCD_BACKLIGHT, HIGH); // turn on the backlight
+    pinMode(LCD_BACKLIGHT, OUTPUT);
+    digitalWrite(LCD_BACKLIGHT, HIGH);
     tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
     tft.setFreeFont(&FreeMono9pt7b);
+
+    // Header
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    tft.drawString("Units: ug/m^3", 10, 5);
+    tft.drawString("AE = Atmospheric environment", 10, 20);
+    tft.drawString("SPM = Std particulate matter", 10, 35);
+    tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+    
+    DEBUG_OUTPUT.println("Setup complete - starting main loop");
 }
 
-
 void loop() {
-    if (sensor.read_sensor_value(buf, 29)) {
-        SERIAL_OUTPUT.println("HM330X read result failed!!");
+    static unsigned long lastUpdate = 0;
+    static int updateCount = 0;
+    
+    // Update alle 1000ms
+    if (millis() - lastUpdate >= 1000) {
+        lastUpdate = millis();
+        updateCount++;
+        
+        DEBUG_OUTPUT.print("Update #");
+        DEBUG_OUTPUT.println(updateCount);
+        
+        // Read sensor data
+        if (sensor.read_sensor_value(buf, 29)) {
+            DEBUG_OUTPUT.println("HM330X read result failed!!");
+        } else {
+            // Parse and display the data
+            parse_result(buf);
+            DEBUG_OUTPUT.println("Display updated successfully");
+        }
     }
-    parse_result_value(buf);
-    parse_result(buf);
-    //SERIAL_OUTPUT.println("");
-    delay(5000);
+    
+    // Refreshrate
+    delay(50);
 }
